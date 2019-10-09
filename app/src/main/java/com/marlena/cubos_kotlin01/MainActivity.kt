c6884c5a7c4efe258b6a38fff1a52d8dc5e2ecba
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2019-10-09 11:17:09 -0300

    Step 2 Activity's

--- app/src/main/java/com/marlena/cubos_kotlin01/MainActivity.kt
+++ app/src/main/java/com/marlena/cubos_kotlin01/MainActivity.kt
@@ -2,11 +2,44 @@ package com.marlena.cubos_kotlin01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Picasso
+            .get()
+            .load("https://amopets.com.br/wp-content/uploads/2018/06/gatos-felizes.gif")
+            .into(imageView)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        tosttest("VOLTEI GALERA,onStart")
+    }
+
+    private fun tosttest(text:String){
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tosttest("VOLTEI GALERA, OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        tosttest("VOLTEI GALERA, OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        tosttest("VOLTEI GALERA, OnStop")
     }
 }
